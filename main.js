488bd86a9e321fd0bd5d36815410c26d53421c95
Azra Ali <azra_ali09@outlook.com>
2025-08-16 13:31:35 -0400

    fixed the button/background video, working on issue with the images not showing up

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var btn = document.getElementById("myBtn");
 function myFunction() {
   if (video.paused) {
     video.play();
-    btn.innerHTML = "Pause";
+    btn.innerHTML = "Play";
   } else {
     video.pause();
     btn.innerHTML = "Play";
