e290e3af7c6cbcc45a93f484e0cd1df17b14914e
Azra Ali <azra_ali09@outlook.com>
2025-08-18 11:15:17 -0400

    added a video/images for markus.html and created a button to play the video

--- mmain.js
+++ mmain.js
@@ -3,5 +3,20 @@ var btn = document.getElementById("myBtn");
 
   btn.onclick = function(){
     window.location.href = "intro.html"
-  }
+  };
 
+
+var video = document.getElementById("Vmarkus");
+var btn1 = document.getElementById("BM");
+
+function myFunction() {
+  if (!video.paused) {
+    video.currentTime= video.duration;
+  } else {
+    video.play();
+  }
+};
+
+  btn.onclick = function(){
+    window.location.href = "intro.html"
+  };
